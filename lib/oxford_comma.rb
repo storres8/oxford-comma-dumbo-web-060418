fcb0a4c434a2452dd82a534891ce9a4d79025d62
storres8 <stevetorres7562@gmail.com>
2018-05-23 23:11:22 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+if array.length == 1
+  array.join
+elsif array.length == 2
+  array.join(" and ")
+elsif array.size >= 3
+  last_word = array.pop()
+  all_other_words = array.join(', ')
+  all_other_words << (", and #{last_word}")
+end
 end
